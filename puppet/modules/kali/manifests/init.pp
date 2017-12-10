3f736d8efbeb4fc31cf0cdb997fc2e9d69e429fb
siavonen <siavonen@users.noreply.github.com>
2017-12-10 20:12:14 +0200

    .

--- puppet/modules/kali/manifests/init.pp
+++ puppet/modules/kali/manifests/init.pp
@@ -4,13 +4,12 @@ class kali {
 	Package { ensure => 'latest', allowcdrom => true, }
 	Exec { path =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ], }
 	
-	file { '/etc/apt/sources.list':
-			ensure => present,
-			}->
-	file_line { 'Append a line to /etc/apt/sources.list':
-				path => '/etc/apt/sources.list',  
-				line => 'deb http://http.kali.org/kali kali-rolling main contrib non-free',
-			}
+	include apt
+
+	apt::source { "Kali_repository":
+        location        => "http://http.kali.org/kali",
+        release         => "The Kali Rolling Repository",
+	}
 
 	exec { 'export DEBIAN_FRONTEND=noninteractive': }
 	
