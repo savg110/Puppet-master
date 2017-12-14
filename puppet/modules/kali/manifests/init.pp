554cd0f43bd4b6734cd021322a3bd4e6fad87853
siavonen <siavonen@users.noreply.github.com>
2017-12-14 10:16:07 +0200

    Update init.pp

--- puppet/modules/kali/manifests/init.pp
+++ puppet/modules/kali/manifests/init.pp
@@ -1,7 +1,5 @@
 class kali {
-	File { owner => '0', group => '0', mode => '0644', }
 	Package { ensure => 'latest', allowcdrom => true, }
-	Exec { path =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ], }
 	
 	
 	package { 'kali-linux-full': }
